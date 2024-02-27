f8544541588ead4a718751247931340cf1cd2eab
candy516004285 <516004285@qq.com>
2024-02-27 22:43:21 +1000

    Initial Commit

---
+++ JPocket/JPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPocketApp.swift
+//  JPocket
+//
+//  Created by Ruby on 27/2/24.
+//
+
+import SwiftUI
+
+@main
+struct JPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
